9196990e8433a43bcbb8a564556f12e39b746785
Rishi <rishi.prabhakar@outlook.com>
2026-07-22 18:30:39 -0700

    create arm test
    Signed-off-by: Rishi <rishi.prabhakar@outlook.com>

--- src/tests/arm_test.py
+++ src/tests/arm_test.py
@@ -4,12 +4,25 @@ from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
-hub = PrimeHub()
+HUB = PrimeHub()
 
 Arm1 = Motor(Port.C)
 Arm2 = Motor(Port.D)
 
-Arm1.run_angle(100,-45)
-# while True:
-#     Arm1.run_angle(500, 360)
-#     Arm2.run_angle(500,360)
+def runCountdown(length : int = 10, warning : int = 3):
+	HUB.light.on(Color.YELLOW)
+	for i in range(length, 0, -1):
+		HUB.display.number(i)
+		print(f"Running in {i} (Total {length}, Warning {warning})")
+		if i == warning:
+			HUB.light.on(Color.ORANGE)
+		wait(1000)
+	HUB.light.on(Color.GREEN)
+		
+while True:
+	Arm1.run_target(500, 0)
+	Arm2.run_target(500, 0)
+	runCountdown(10, 3)
+	Arm1.run_target(500, 90)
+	Arm2.run_target(500, 90)
+	runCountdown(10, 3)
